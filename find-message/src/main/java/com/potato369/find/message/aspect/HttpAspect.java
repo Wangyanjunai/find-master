312f9a0d6454561f4e9117f2c39542fddb3e75d1
Jack <406151651@qq.com>
2021-06-06 23:38:44 +0800

    修改用户模块用户注册接口代码。

---
+++ find-message/src/main/java/com/potato369/find/message/aspect/HttpAspect.java
@@ -0,0 +1,47 @@
+package com.potato369.find.message.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Slf4j
+@Component
+public class HttpAspect {
+
+    @Pointcut("execution(public * com.potato369.find.message.controller.*.*(..))")
+    public void log() {
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        //url
+        log.info("url={}", request.getRequestURL());
+        //method
+        log.info("method={}", request.getMethod());
+        //ip
+        log.info("ip={}", request.getRemoteAddr());
+        //类方法
+        log.info("class method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        //参数
+        log.info("args={}", joinPoint.getArgs());
+    }
+
+    @After("log()")
+    public void doAfter() {
+
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        log.info("response={}", object.toString());
+    }
+}
