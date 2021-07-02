5e4a792b6a37906f596d5cd527c7d1ea9d905f8c
Jack <406151651@qq.com>
2021-07-02 19:40:07 +0800

    修改动态模块标签相关接口代码

--- find-dynamic/src/test/java/com/potato369/find/dynamic/DynamicServiceApplicationTests.java
+++ find-dynamic/src/test/java/com/potato369/find/dynamic/DynamicServiceApplicationTests.java
@@ -11,6 +11,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest(classes = DynamicServiceApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -25,7 +29,10 @@ public class DynamicServiceApplicationTests {
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("find/v1/hello/say.do").accept(MediaType.APPLICATION_JSON));
+        mockMvc.perform(MockMvcRequestBuilders.get("/find/v1/hello/say.do")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World !")));
     }
 
     @Test
