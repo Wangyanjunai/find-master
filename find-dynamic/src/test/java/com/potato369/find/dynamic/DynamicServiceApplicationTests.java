246184956a34352cae3d24309f6fec5ed179820e
Jacky <1515232994@qq.com>
2021-07-03 23:48:18 +0800

    修改测试代码

--- find-dynamic/src/test/java/com/potato369/find/dynamic/DynamicServiceApplicationTests.java
+++ find-dynamic/src/test/java/com/potato369/find/dynamic/DynamicServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,8 +15,8 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = DynamicServiceApplication.class)
-@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class DynamicServiceApplicationTests {
 
