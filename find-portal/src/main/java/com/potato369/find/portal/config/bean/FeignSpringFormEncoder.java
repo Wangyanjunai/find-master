37bb6537e00b720b0d6c8279d7052525e5faffd3
Jack <406151651@qq.com>
2021-07-21 21:53:44 +0800

    修改代码

--- find-portal/src/main/java/com/potato369/find/portal/config/bean/FeignSpringFormEncoder.java
+++ find-portal/src/main/java/com/potato369/find/portal/config/bean/FeignSpringFormEncoder.java
@@ -15,38 +15,36 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * author：YZH
- * time: 2019/4/29 15:35
- * description: 多文件上传配置
+ * author：YZH time: 2019/4/29 15:35 description: 多文件上传配置
  * https://www.cnblogs.com/cq-yangzhou/p/10791008.html
  **/
 public class FeignSpringFormEncoder extends FormEncoder {
+	public FeignSpringFormEncoder() {
+		this(new Default());
+	}
 
-    public FeignSpringFormEncoder() {
-        this(new Default());
-    }
+	public FeignSpringFormEncoder(Encoder delegate) {
+		super(delegate);
+		MultipartFormContentProcessor processor = (MultipartFormContentProcessor) this
+				.getContentProcessor(ContentType.MULTIPART);
+		processor.addWriter(new SpringSingleMultipartFileWriter());
+		processor.addWriter(new SpringManyMultipartFilesWriter());
+	}
 
-    public FeignSpringFormEncoder(Encoder delegate) {
-        super(delegate);
-        MultipartFormContentProcessor processor = (MultipartFormContentProcessor) this.getContentProcessor(ContentType.MULTIPART);
-        processor.addWriter(new SpringSingleMultipartFileWriter());
-        processor.addWriter(new SpringManyMultipartFilesWriter());
-    }
-
-    public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
-        if (bodyType.equals(MultipartFile.class)) {
-            MultipartFile file = (MultipartFile) object;
-            Map<String, Object> data = Collections.singletonMap(file.getName(), object);
-            super.encode(data, MAP_STRING_WILDCARD, template);
-            return;
-        } else if (bodyType.equals(MultipartFile[].class)) {
-            MultipartFile[] file = (MultipartFile[]) object;
-            if (file != null) {
-                Map<String, Object> data = Collections.singletonMap(file.length == 0 ? "" : file[0].getName(), object);
-                super.encode(data, MAP_STRING_WILDCARD, template);
-                return;
-            }
-        }
-        super.encode(object, bodyType, template);
-    }
+	public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
+		if (bodyType.equals(MultipartFile.class)) {
+			MultipartFile file = (MultipartFile) object;
+			Map<String, Object> data = Collections.singletonMap(file.getName(), object);
+			super.encode(data, MAP_STRING_WILDCARD, template);
+			return;
+		} else if (bodyType.equals(MultipartFile[].class)) {
+			MultipartFile[] file = (MultipartFile[]) object;
+			if (file != null) {
+				Map<String, Object> data = Collections.singletonMap(file.length == 0 ? "" : file[0].getName(), object);
+				super.encode(data, MAP_STRING_WILDCARD, template);
+				return;
+			}
+		}
+		super.encode(object, bodyType, template);
+	}
 }
