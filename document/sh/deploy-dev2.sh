fe2af7d75ae7287b4d61a625c7a5786dbf24c9f9
Jack <406151651@qq.com>
2021-07-31 15:00:05 +0800

    修改项目配置。

--- document/sh/deploy-dev2.sh
+++ document/sh/deploy-dev2.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#需要配置如下参数
-#项目路径,在ExecuteShell中配置项目路径,pwd就可以获得该项目路径
-#export PROJ_PATH=这个jenkins任务在部署机器上的路径
-# shellcheck disable=SC2046
-kill -9 $(ps -ef | grep java | grep find | awk '{print $2}')
-cd ~/prod/find-master || exit
+kill -9 $(ps -ef | grep java | awk '{print $2}')
+cd ~/IdeaProjects/find-master || exit
 git pull
 mvn clean package
 rm -rf ~/jar/*.jar
 mv find-admin/target/*.jar ~/jar/
-mv find-config/target/*.jar ~/jar/
 mv find-dynamic/target/*.jar ~/jar/
 mv find-message/target/*.jar ~/jar/
 mv find-order/target/*.jar ~/jar/
@@ -22,4 +17,3 @@ nohup java -jar -server -Xmx128m -Xss16m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConc
 nohup java -jar -server -Xmx128m -Xss16m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=dev2 -Dserver.port=8084 find-portal-8084.jar >find-portal-8084.log 2>&1 &
 nohup java -jar -server -Xmx128m -Xss16m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=dev2 -Dserver.port=8085 find-admin-8085.jar >find-admin-8085.log 2>&1 &
 nohup java -jar -server -Xmx128m -Xss16m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=dev2 -Dserver.port=8086 find-order-8086.jar >find-order-8086.log 2>&1 &
-nohup java -jar -server -Xmx128m -Xss16m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=dev2 -Dserver.port=8087 find-config-8087.jar >find-config-8087.log 2>&1 &
