9a181350547523f7053ff2617484b6b9738cb87c
Jack <406151651@qq.com>
2021-08-13 11:54:11 +0800

    修改后台管理模块代码。

--- document/sh/deploy-test.sh
+++ document/sh/deploy-test.sh
@@ -13,10 +13,10 @@ mv find-order/target/*.jar ~/jar
 mv find-portal/target/*.jar ~/jar
 mv find-user/target/*.jar ~/jar
 cd ~/jar || exit
-nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8091 find-user-8081.jar >find-user-8081.log 2>&1 &
-nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8092 find-message-8082.jar >find-message-8082.log 2>&1 &
-nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8093 find-dynamic-8083.jar >find-dynamic-8083.log 2>&1 &
-nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8094 find-portal-8084.jar >find-portal-8084.log 2>&1 &
+nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8081 find-user-8081.jar >find-user-8081.log 2>&1 &
+nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8082 find-message-8082.jar >find-message-8082.log 2>&1 &
+nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8083 find-dynamic-8083.jar >find-dynamic-8083.log 2>&1 &
+nohup java -jar -server Xmx128m -Xss32m -Xms4m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8084 find-portal-8084.jar >find-portal-8084.log 2>&1 &
 #nohup java -jar -server -Xmx64m -Xss8m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8085 find-admin-8085.jar >find-admin-8085.log 2>&1 &
 #nohup java -jar -server -Xmx64m -Xss8m -Xms2m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dcom.alibaba.nacos.client.naming.ctimeout=5000 -Dspring.profiles.active=test -Dserver.port=8086 find-order-8086.jar >find-order-8086.log 2>&1 &
 exit 0
