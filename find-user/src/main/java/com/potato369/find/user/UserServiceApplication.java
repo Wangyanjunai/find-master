ba3a0c01e5b73f9eb128d638d6314c8c4eecd4ca
Jack <406151651@qq.com>
2021-09-10 11:48:48 +0800

    1、）修改项目日志配置

--- find-user/src/main/java/com/potato369/find/user/UserServiceApplication.java
+++ find-user/src/main/java/com/potato369/find/user/UserServiceApplication.java
@@ -3,12 +3,10 @@ package com.potato369.find.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringCloudApplication
-@EnableFeignClients(basePackages = {"com.potato369.find.user.feign"})
 @MapperScan(basePackages = {"com.potato369.find.mbg"})
 public class UserServiceApplication {
 
